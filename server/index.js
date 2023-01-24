af68d6b8a7a870b2d7f4fd63e8a098ddadadfca1
dmckenna44 <dmckenna05@gmail.com>
2023-01-24 12:14:12 -0800

    more configuration of session

--- server/index.js
+++ server/index.js
@@ -19,10 +19,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.set('trust proxy');
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
+  proxy: true,
   name: 'g2g-session-cookie',
   cookie: { maxAge: 1200000 },
   sameSite: 'none'
